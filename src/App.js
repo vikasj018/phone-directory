484b209ba74ee2f78898316be09d4ace20ba00ec
vikasj018 <vikasjaiprakash.18@gmail.com>
2019-07-25 21:47:28 +0530

    “Styled ‘Header’ (Inline Styling)”

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React, { Component } from 'react';
+import Header from "./Header"
 
 class App extends Component {
   render() {
+
     return (
+    <div>
+      <Header />
+      <button> Add </button>
       <div>
-        {/* <div className="header">
-          Phone Directory
-      </div>
-        <button> Add </button>
-        <div>
-          <span>Name</span><br />
-          <span>Phone</span>
-        </div> */}
-        <label htmlFor="name"> Name: </label>
-        <input id="name" type="text" placeholder="Type Here" defaultValue="Vikas"/>
-      </div>
+        <span>Name</span><br />
+        <span>Phone</span>
+      </div> 
+    </div>
+    
     );
   }
 }
