bf263694b34baabf1010f688f1f28e8b5d7b0eb4
vikasj018 <vikasjaiprakash.18@gmail.com>
2019-07-26 21:14:21 +0530

    Added ‘Delete’ button

--- src/App.js
+++ src/App.js
@@ -28,13 +28,15 @@ class App extends Component {
           <div className="grid-container heading-container">
             <span className="grid-item name-heading">Name</span>
             <span className="grid-item phone-heading">Phone</span>
+            
           </div>
         
         {
           subscribers.map(sub => {
             return <div key={sub.id} className="grid-container">
-              <span className="grid-item">(sub.name)</span>
-              <span className="grid-item">(sub.phone)</span>
+              <span className="grid-item">(sub.name)</span>              
+              <span className="grid-item">(sub.phone)</span>   
+              <button className="custom-btn delete-btn">Delete</button>    
             </div>
           })
         }
